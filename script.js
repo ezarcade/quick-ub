eaa1c0075f80a8db857ac42fc203f0f161baa793
ezarcade <128180362+ezarcade@users.noreply.github.com>
2023-05-09 08:45:31 -0700

    fix

---
+++ script.js
@@ -0,0 +1,19 @@
+var frame = document.getElementById("frame");
+
+var btn1 = document.getElementById("btn1");
+var btn2 = document.getElementById("btn2");
+var btn3 = document.getElementById("btn3");
+
+btn1.addEventListener("click",link1)
+btn2.addEventListener("click",link2)
+btn3.addEventListener("click",link3)
+
+function link1(){
+  frame.src="https://pixabay.com/photo-2845763/"
+}
+function link2(){
+  frame.src="https://pixabay.com/photo-3126513/"
+}
+function link3(){
+  frame.src="https://pixabay.com/photo-3114729/"
+}
