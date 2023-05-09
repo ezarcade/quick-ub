a0ed2588465241b8146a8bbad3dbebea1206ceb6
ezarcade <128180362+ezarcade@users.noreply.github.com>
2023-05-09 08:57:24 -0700

    fix

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
-var frame = document.getElementById("frame");
+function newSite1() {
+    var sites = ['https://spew45.github.io']
 
-var btn1 = document.getElementById("btn1");
-var btn2 = document.getElementById("btn2");
-var btn3 = document.getElementById("btn3");
-
-btn1.addEventListener("click",link1)
-btn2.addEventListener("click",link2)
-btn3.addEventListener("click",link3)
-
-function link1(){
-  frame.src="https://spew45.github.io/"
+    document.getElementById('myIframe').src = sites[Math.floor(Math.random() * sites.length)];
+    alert('Done');
 }
-function link2(){
-  frame.src="https://pixabay.com/photo-3126513/"
-}
-function link3(){
-  frame.src="https://pixabay.com/photo-3114729/"
+function newSite2() {
+    var sites = ['https://ezarcade.github.io/']
+
+    document.getElementById('myIframe').src = sites[Math.floor(Math.random() * sites.length)];
+    alert('Done');
 }
