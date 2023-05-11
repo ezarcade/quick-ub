8942b600e927c41a7b62ca85d8974a3851f90f79
ezarcade <128180362+ezarcade@users.noreply.github.com>
2023-05-11 10:54:44 -0700

    fix

--- script.js
+++ script.js
@@ -2,11 +2,9 @@ function newSite1() {
     var sites = ['https://spew45.github.io']
 
     document.getElementById('myIframe').src = sites[Math.floor(Math.random() * sites.length)];
-    alert('Done');
 }
 function newSite2() {
     var sites = ['https://ezarcade.github.io/']
 
     document.getElementById('myIframe').src = sites[Math.floor(Math.random() * sites.length)];
-    alert('Done');
 }
